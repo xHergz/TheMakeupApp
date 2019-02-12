c5a75e26f787aa7c0ef0d0c51f9322fd735cfd4f
Justin <xhergz@gmail.com>
2019-02-12 15:39:21 -0500

    Added config file to repo, changed functions to be compatible with mysql 8

---
+++ Config/DeployConfigsLocally.bat
@@ -0,0 +1,4 @@
+rd "C:\Program Files\HerWal\TheMakeupApp\Config" /s /q
+
+mkdir "C:\Program Files\HerWal\TheMakeupApp\Config"
+copy ".\DatabaseConnection.config" "C:\Program Files\HerWal\TheMakeupApp\Config\DatabaseConnection.config"
