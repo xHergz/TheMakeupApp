4e00ec232107d649d73eca433387390e31cae056
Keith <keithgwallace@gmail.com>
2019-03-10 21:29:00 -0400

    Reducers and Room Setup
    Audio/Video Reducers set along with room launching home page. Room launcher is not loading properly at the moment.

---
+++ Website/React/TheMakeupApp/components/InstantConsultSearchHome.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const InstantSearchHome = props =>
+
+<div className="instantsearchhome">
+    <div>
+      <h1 itemProp="headline">Webrtc Video Room</h1>
+      <p>Please enter a room name.</p>
+      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
+      <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
+      <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link>
+      { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
+      { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
+    </div>
+  </div>;
+
+  InstantSearchHome.propTypes = {
+    handleChange: PropTypes.func.isRequired,
+    defaultRoomId: PropTypes.string.isRequired,
+    roomId: PropTypes.string.isRequired,
+    rooms: PropTypes.array.isRequired
+
+  };
+
+  const mapStateToProps = store => ({rooms: store.rooms});
+
+  export default connect(mapStateToProps)(InstantSearchHome);
--- Website/React/TheMakeupApp/index.js
+++ Website/React/TheMakeupApp/index.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styles from '../Common/styles/Chat.css'
 import TheMakeupApp from './containers/TheMakeupApp';
 import configureStore from './store/configureStore';
 
@@ -12,7 +13,11 @@ const store = configureStore();
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
-            <TheMakeupApp />
+            <TheMakeupApp>
+            <Switch>
+                
+            </Switch>
+            </TheMakeupApp>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
