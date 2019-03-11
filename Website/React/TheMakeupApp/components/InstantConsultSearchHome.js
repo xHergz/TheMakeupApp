a1e57f053154b20b8134e29c316bdbb745348da1
Keith <keithgwallace@gmail.com>
2019-03-11 09:24:40 -0400

    Hanging Fixed

--- Website/React/TheMakeupApp/components/InstantConsultSearchHome.js
+++ Website/React/TheMakeupApp/components/InstantConsultSearchHome.js
@@ -11,9 +11,7 @@ const InstantSearchHome = props =>
       <p>Please enter a room name.</p>
       <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
       <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
-      <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link>
-      { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
-      { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
+
     </div>
   </div>;
 
